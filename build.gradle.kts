e628c9e9dd84c2c42ed8dc10a168fd5e4188500e
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-07 14:07:39 -0500

    :arrow_up: scs 0.21.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ mainDependencies {
 
 visualizersDependencies {
    api(ihmc.sourceSetProject("main"))
-   api("us.ihmc:simulation-construction-set:0.21.16")
+   api("us.ihmc:simulation-construction-set:0.21.18")
 }
 
 app.entrypoint("MicroStrain3DMVisualizer", "us.ihmc.visualizers.sensors.imu.lord.microstrain.MicroStrain3DMVisualizer")
